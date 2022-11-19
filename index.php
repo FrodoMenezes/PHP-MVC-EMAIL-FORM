c2a1b75f3784f05c769044330de65772356004d3
Eduardo Menezes <79610098+FrodoMenezes@users.noreply.github.com>
2022-11-19 18:52:05 -0300

    MVC structure
    - File htaccess conifg added;
    - Initial testes on index.php;
    - Router and MySql;

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	define('HOST','localhost');
+	define('DATABASE','db_php_vanilla');
+	define('USER','root');
+	define('PASSWORD','');
+
+	$autoload = function($class){
+		include($class.'.php');
+	};
+
+//	$pdo = \MySql::conectar();
+
+	spl_autoload_register($autoload);
+
+	Router::get('/home/',function(){
+		echo '<h2>Home!</h2>';
+	});
+
+	Router::get('/home/?',function($par){
+		echo $par[2];
+	});
+
+?>
